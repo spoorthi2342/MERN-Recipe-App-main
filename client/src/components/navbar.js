8b499f71bfe5c25baf643ee4339d15ff1378ad11
spoorthi <spoorthiachar9@gmail.com>
2023-09-03 17:29:56 +0530

    fifth commit

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faPlus, faBookmark, faSignInAlt, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
@@ -11,15 +13,31 @@ export const Navbar = () => {
     window.localStorage.clear();
     navigate("/auth");
   };
+
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/create-recipe">Create Recipe</Link>
-      <Link to="/saved-recipes">Saved Recipes</Link>
+      <Link to="/" className="nav-link">
+        <FontAwesomeIcon icon={faHome} className="nav-icon" />
+        Home
+      </Link>
+      <Link to="/create-recipe" className="nav-link">
+        <FontAwesomeIcon icon={faPlus} className="nav-icon" />
+        Create Recipe
+      </Link>
+      <Link to="/saved-recipes" className="nav-link">
+        <FontAwesomeIcon icon={faBookmark} className="nav-icon" />
+        Saved Recipes
+      </Link>
       {!cookies.access_token ? (
-        <Link to="/auth">Login/Register</Link>
+        <Link to="/auth" className="nav-link">
+          <FontAwesomeIcon icon={faSignInAlt} className="nav-icon" />
+          Login/Register
+        </Link>
       ) : (
-        <button onClick={logout}> Logout </button>
+        <button onClick={logout} className="logout-button">
+          <FontAwesomeIcon icon={faSignOutAlt} className="nav-icon" />
+          Logout
+        </button>
       )}
     </div>
   );
