de7e896d2001c9d45d22f2129255f65919e2bc26
spoorthi <spoorthiachar9@gmail.com>
2023-09-03 12:04:28 +0530

    third commit

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -52,8 +52,24 @@ export const Home = () => {
       console.log(e.message)
     }
   }
+  const getShareableRecipeUrl = (recipeID) => {
+    // Replace with your actual domain and route for viewing recipes
+    const baseUrl = "http://localhost:3000/recipes";
+    return `${baseUrl}/${recipeID}`;
+  };
 
   const isRecipeSaved = (id) => savedRecipes.includes(id);
+  const copyRecipeLinkToClipboard = (recipeID) => {
+    const shareableUrl = getShareableRecipeUrl(recipeID);
+
+    navigator.clipboard.writeText(shareableUrl)
+      .then(() => {
+        alert("Recipe link copied to clipboard!");
+      })
+      .catch((error) => {
+        console.error("Copy to clipboard failed:", error);
+      });
+  };
 
   return (
     <div>
@@ -61,17 +77,26 @@ export const Home = () => {
       <ul>
         {recipes.map((recipe) => (
           <li key={recipe._id}>
+         
             <div>
               <h2>{recipe.name}</h2>
               <button
                 onClick={() => saveRecipe(recipe._id)}
                 disabled={isRecipeSaved(recipe._id)}
+                
               >
+              
+                
                 {isRecipeSaved(recipe._id) ? "Saved" : "Save"}
               </button>
-              <button type="button" onClick={() => deleter(recipe._id)}>
+              <button type="button" onClick={() => deleter(recipe._id)}
+              >
                 delete
               </button>
+              <button onClick={() => copyRecipeLinkToClipboard(recipe._id)}
+              >
+                    Share
+                  </button>
             </div>
             <div className="instructions">
               <p>{recipe.instructions}</p>
